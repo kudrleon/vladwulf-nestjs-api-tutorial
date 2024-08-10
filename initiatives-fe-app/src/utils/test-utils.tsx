91a966ba05fd3529a4b0e33032b10b7c60c02340
Deema <dmytro@mathpix.com>
2024-08-10 15:50:28 -0400

    Cleans up boilerplate

--- initiatives-fe-app/src/utils/test-utils.tsx
+++ initiatives-fe-app/src/utils/test-utils.tsx
@@ -1,10 +1,11 @@
+import type { AppStore, RootState } from "../app/store"
+import type { PropsWithChildren, ReactElement } from "react"
+
+import { Provider } from "react-redux"
 import type { RenderOptions } from "@testing-library/react"
+import { makeStore } from "../app/store"
 import { render } from "@testing-library/react"
 import userEvent from "@testing-library/user-event"
-import type { PropsWithChildren, ReactElement } from "react"
-import { Provider } from "react-redux"
-import type { AppStore, RootState } from "../app/store"
-import { makeStore } from "../app/store"
 
 /**
  * This type extends the default options for
